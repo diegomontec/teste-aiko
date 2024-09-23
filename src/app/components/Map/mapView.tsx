bb6a4d6c844a9ea6b78a6f557f43fafd67151a3b
diegomcs <diegocs.art@gmail.com>
2024-09-23 12:23:07 -0300

    Correção Map

--- src/app/components/Map/mapView.tsx
+++ src/app/components/Map/mapView.tsx
@@ -6,45 +6,59 @@ import { useEffect, useState } from "react";
 import { setupMap, addMarkersToMap } from "@/app/utils/mapUtils";
 import { useFilterStore } from "@/app/store/FilterStore";
 import Modal from "@/app/components/Modal";
-import { Equipment, EquipmentModel, EquipmentState, EquipmentStateHistory } from "@/app/types/dataTypes"; 
+import {
+  Equipment,
+  EquipmentModel,
+  EquipmentState,
+  EquipmentStateHistory,
+} from "@/app/types/dataTypes";
 import mapboxgl from "mapbox-gl";
-import equipmentModels from "@/app/data/equipmentModel.json"; 
-import equipmentStates from "@/app/data/equipmentState.json"; 
-import equipmentStateHistories from "@/app/data/equipmentStateHistory.json"; 
+import equipmentModels from "@/app/data/equipmentModel.json";
+import equipmentStates from "@/app/data/equipmentState.json";
+import equipmentStateHistories from "@/app/data/equipmentStateHistory.json";
 
 const MapView = () => {
   const { stateFilters, modelFilters } = useFilterStore();
   const [isModalOpen, setModalOpen] = useState(false);
-  const [selectedEquipment, setSelectedEquipment] = useState<Equipment | null>(null);
-  const [selectedEquipmentModel, setSelectedEquipmentModel] = useState<EquipmentModel | null>(null);
-  const [selectedEquipmentState, setSelectedEquipmentState] = useState<EquipmentState | null>(null);
-  const [equipmentStateHistory, setEquipmentStateHistory] = useState<EquipmentStateHistory | null>(null);
+  const [selectedEquipment, setSelectedEquipment] = useState<Equipment | null>(
+    null
+  );
+  const [selectedEquipmentModel, setSelectedEquipmentModel] =
+    useState<EquipmentModel | null>(null);
+  const [selectedEquipmentState, setSelectedEquipmentState] =
+    useState<EquipmentState | null>(null);
+  const [equipmentStateHistory, setEquipmentStateHistory] =
+    useState<EquipmentStateHistory | null>(null);
 
   useEffect(() => {
     const map = setupMap(process.env.NEXT_PUBLIC_MAPBOX_TOKEN || "");
 
     map.addControl(new mapboxgl.NavigationControl());
-
     addMarkersToMap(
       map,
       stateFilters,
       modelFilters,
-      (equipment: Equipment) => {
-        setSelectedEquipment(equipment),
-      setModalOpen(true)
+      (equipment: Equipment | null) => {
+        setSelectedEquipment(equipment); // Agora aceita null
+        if (equipment) {
+          // Verifique se equipment não é null
+          const equipmentModel = equipmentModels.find(
+            (model: { id: string }) => model.id === equipment.equipmentModelId
+          );
+          setSelectedEquipmentModel(equipmentModel || null);
 
-        const equipmentModel = equipmentModels.find(
-          (model: { id: string; }) => model.id === equipment.equipmentModelId
-        );
-        setSelectedEquipmentModel(equipmentModel || null);
+          const stateHistory = equipmentStateHistories.find(
+            (history: { equipmentId: string }) =>
+              history.equipmentId === equipment.id
+          );
+          setEquipmentStateHistory(stateHistory || null);
 
-        const stateHistory = equipmentStateHistories.find(
-          (history: { equipmentId: string; }) => history.equipmentId === equipment.id
-        );
-        setEquipmentStateHistory(stateHistory || null);
-
-        setModalOpen(true);
-      }
+          setModalOpen(true);
+        } else {
+          setModalOpen(false); // Fecha a modal se equipment for null
+        }
+      },
+      setModalOpen
     );
 
     return () => {
@@ -62,7 +76,6 @@ const MapView = () => {
           equipment={selectedEquipment}
           equipmentModel={selectedEquipmentModel}
           equipmentStateHistory={equipmentStateHistory}
-          // eslint-disable-next-line react/jsx-no-duplicate-props
           equipmentStates={equipmentStates}
         />
       )}
